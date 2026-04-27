e8a9c9b262fd4327635782e9e3f5d2e06a33f98b
Joel <jsa5214@office.proven.cat>
2026-04-27 19:57:25 +0200

    small fixes2

--- security.tf
+++ security.tf
@@ -12,11 +12,18 @@ resource "aws_security_group" "wireguard" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    description = "SSH abierto temporalmente (desarrollo)"
+    description = "SSH solo desde el tunel VPN"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.8.0.0/24"]
+  }
+  ingress {
+    description = "Trafico interno VPC (forwarding desde subnets privadas)"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
   }
   egress {
     from_port   = 0
